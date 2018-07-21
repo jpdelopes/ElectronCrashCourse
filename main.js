0dbed46049969da77872c66f33f4449d22a465ea
jpdelopes <joaopedrodelopes@gmail.com>
2018-07-21 02:53:41 +0100

    Installed electron and axios successfully w/  npm install --global electron-download and sudo npm install --save axios --global; Added HTML and CSS.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 const url = require('url')
+const shell = require('electron').shell
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,7 +12,7 @@ function createWindow () {
     win = new BrowserWindow({width: 800, height: 600})
 
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'src/index.html'),
         protocol: 'file:',
         slashes: true
     }))
@@ -29,6 +30,33 @@ function createWindow () {
         // when you should delete the corresponding element.
         win = null
     })
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu: [
+                {label: 'Adjust Notification Value'},
+                {
+                    label: 'CoinMarketCap',
+                    click() {
+                        shell.openExternal('http://coinmarketcap.com')
+                    }
+                },
+                {type: 'separator'},
+                {
+                    label: 'Exit',
+                    click()  {
+                        app.quit()
+                    }
+                }
+            ]
+        },
+        {
+            label: 'Info'
+        }
+    ])
+
+    Menu.setApplicationMenu(menu);  
 }
 
 // This method will be called when Electron has finished
