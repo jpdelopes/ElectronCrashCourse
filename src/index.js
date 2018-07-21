2ed5119d1d0bafa885f6afe3a7d27d0571d1e5b6
jpdelopes <joaopedrodelopes@gmail.com>
2018-07-21 05:20:00 +0100

    Added desktop notifications.

--- src/index.js
+++ src/index.js
@@ -12,17 +12,28 @@ const ipc = electron.ipcRenderer
 const notifyBtn = document.getElementById('notifyBtn')
 var price = document.querySelector('h1')
 var targetPrice = document.getElementById('targetPrice')
+var targetPriceVal
+
+const notification = {
+    title: 'BTC Alert',
+    body: 'BTC just beat your target price!',
+    icon: path.join(__dirname, '../assets/images/btc.png')
+}
 
 function getBTC() {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=EUR')
         .then(res => {
             const cryptos = res.data.BTC.EUR
             price.innerHTML = '€'+cryptos.toLocaleString('en')
+
+            if(targetPrice.innerHTML != '' && targetPriceVal < res.data.BTC.EUR)    {
+                const myNotification = new window.Notification(notification.title, notification)
+            }
         })
 }
 
 getBTC()
-setInterval(getBTC, 30000);
+setInterval(getBTC, 6000);
 
 notifyBtn.addEventListener('click', function(event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
